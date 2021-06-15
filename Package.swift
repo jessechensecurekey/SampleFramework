85a0f8a8075215faa8794d5d340ac935d2ab6043
jessechensecurekey <85701465+jessechensecurekey@users.noreply.github.com>
2021-06-15 16:30:58 -0400

    Update Package.swift
    add onfido

--- Package.swift
+++ Package.swift
@@ -18,6 +18,11 @@ let package = Package(
         .binaryTarget(
                     name: "SampleFramework",
                     path: "SampleFramework.xcframework"
-                )
+                ),
+         .binaryTarget(
+            name: "Onfido",
+            url: "https://s3-eu-west-1.amazonaws.com/onfido-sdks/ios/Onfido-v22.0.0.zip",
+            checksum: "11c723ef63705fab57678e713dcb8796dd4b45e890744d4de5e9736e65ab2f4c"
+        )
     ]
 )
