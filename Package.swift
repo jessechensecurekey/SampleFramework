cdbd4c75682d09eb9e82a77a811a0dadb5a73088
jessechensecurekey <85701465+jessechensecurekey@users.noreply.github.com>
2021-06-15 16:02:13 -0400

    Update Package.swift

--- Package.swift
+++ Package.swift
@@ -17,7 +17,7 @@ let package = Package(
         // Targets can depend on other targets in this package, and on products in packages this package depends on.
         .binaryTarget(
                     name: "SampleFramework",
-                    path: "Library/SampleFramework.xcframework"
+                    path: "SampleFramework.xcframework"
                 )
     ]
 )
