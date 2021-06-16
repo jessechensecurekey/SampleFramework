c035f1665f00a7b1058575cfce1f7343167edd6d
jessechensecurekey <85701465+jessechensecurekey@users.noreply.github.com>
2021-06-15 21:53:54 -0400

    Update Package.swift
    use private repo to link xcframework zip

--- Package.swift
+++ Package.swift
@@ -19,10 +19,11 @@ let package = Package(
         // Targets are the basic building blocks of a package. A target can define a module or a test suite.
         // Targets can depend on other targets in this package, and on products in packages this package depends on.
         .binaryTarget(
-                    name: "SampleFramework",
-                    path: "SampleFramework.xcframework"
-                ),
-         .binaryTarget(
+            name: "SampleFramework",
+            url: "https://github.com/jessechensecurekey/SampleFramework/raw/master/SampleFramework.xcframework.zip",
+            checksum: "83e38bf4078ead351ebae49d7b4362ab783ce0615f29d4c08236868a9216fb6a"
+        ),
+        .binaryTarget(
             name: "Onfido",
             url: "https://s3-eu-west-1.amazonaws.com/onfido-sdks/ios/Onfido-v22.0.0.zip",
             checksum: "11c723ef63705fab57678e713dcb8796dd4b45e890744d4de5e9736e65ab2f4c"
